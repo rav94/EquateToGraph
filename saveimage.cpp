6a32c6c01b9b28c7ed88455f6d81971acebf4aa5
Ravindu <fernandoravindu1994@gmail.com>
2015-12-19 11:39:47 +0530

    Addition of SaveImage class. (Source and Header Files) @rav94

---
+++ saveimage.cpp
@@ -0,0 +1,23 @@
+#include "saveimage.h"
+#include <QString>
+#include <QPixmap>
+#include <QFileDialog>
+#include <QCoreApplication>
+
+
+SaveImage::SaveImage()
+{
+
+}
+
+void SaveImage::SaveAsImage()
+{
+//    QString fileName= QFileDialog::getSaveFileName(this, "Save As an Image", QCoreApplication::applicationDirPath(), "BMP Files (.bmp);;JPEG (.JPEG);;PNG (*.png)" );
+//           if (!fileName.isNull())
+//           {
+
+//                    QPixmap pixMap = ui->customPlot->grab();
+//                    pixMap.save(fileName);
+//           }
+}
+
---
+++ saveimage.h
@@ -0,0 +1,15 @@
+#ifndef SAVEIMAGE_H
+#define SAVEIMAGE_H
+
+
+class SaveImage
+{
+public:
+    SaveImage();
+    void SaveAsImage();
+
+private:
+
+};
+
+#endif // SAVEIMAGE_H
